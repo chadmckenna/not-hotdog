8e8e80ef64456a2958d35aa1e9f8b4895746c7b8
Chad McKenna <chad.mckenna@welltok.com>
2018-01-08 09:56:54 -0700

    Adding CORS

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, redirect, url_for, jsonify
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from model import predict as predict_simple
 from model_connected import predict as predict_connected
@@ -11,6 +12,8 @@ ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+CORS(app)
+
 @app.route('/')
 def index():
     return 'Hello, Hotdog!'
@@ -19,7 +22,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/is_hotdog', methods=['POST'])
+@app.route('/is_hotdog', methods=['OPTIONS', 'POST'])
 def is_hotdog():
     print(request.files)
     if 'file' not in request.files:
@@ -35,11 +38,9 @@ def is_hotdog():
         file.save(file_path)
         predictions_1 = predict_simple(file_path)
         predictions_2 = predict_connected(file_path)
-        return jsonify({
-            'model1':
+        return jsonify([
                 {'what': predictions_1[0], 'probability': predictions_1[1].tolist()},
-            'model2':
                 {'what': predictions_2[0], 'probability': predictions_2[1].tolist()}
-            })
+            ])
 
     return 'Nope'
